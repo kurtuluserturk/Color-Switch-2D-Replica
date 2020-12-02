ccee00e14baa19458ff20ae3289833e1ab64c103
Kurtuluş ERTÜRK <47227072+kurtuluserturk@users.noreply.github.com>
2020-12-02 18:23:49 +0300

    2D Game Replica
    It was made to practise.

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float jumpForce = 8f;
+    public Rigidbody2D rb;
+    public SpriteRenderer sr;
+    public string currentColor;
+    public Color colorCyan;
+    public Color colorYellow;
+    public Color colorMagenta;
+    public Color colorPink;
+    void Start()
+    {                             
+        Physics2D.gravity = new Vector2(0, 0);
+        SetRandomColor();        
+    }
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            Physics2D.gravity = new Vector2(0, -30);
+        }                
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "ColorChanger")
+        {
+            SetRandomColor();
+            Destroy(col.gameObject);
+            return;     
+        }
+        if (col.tag != currentColor)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }     
+    }
+    void SetRandomColor()
+    {
+        int index = Random.Range(0, 4);    
+        switch (index)
+        {
+            case 0:
+                currentColor = "Cyan";
+                sr.color = colorCyan;
+                break;
+            case 1:
+                currentColor = "Yellow";
+                sr.color = colorYellow;
+                break;
+            case 2:
+                currentColor = "Magenta";
+                sr.color = colorMagenta;
+                break;
+            case 3:
+                currentColor = "Pink";
+                sr.color = colorPink;
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerFollower.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerFollower : MonoBehaviour
+{
+    public Transform player;
+    void Update()
+    {
+        if (player.position.y > transform.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, player.position.y, transform.position.z);
+        }
+    }
+}
